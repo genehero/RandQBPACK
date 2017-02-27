5da96cafed910f44feefa9734ae0f8f1deed7378
冯栩 <391830236@qq.com>
2017-02-27 10:45:30 +0800

    Some modification

--- Matlab/adpRandSVD.m
+++ Matlab/adpRandSVD.m
@@ -1,5 +1,6 @@
 function [Q, B, V] = adpRandSVD(A, tol, p, block_size)
-%Adaptive truncated SVD of A for the given error tolerance.   
+%Adaptive truncated SVD of A for the given error tolerance. 
+%Suitable for a dense/sparse A, can be faster than the randQB for sparse A.
 %Syntax:
 % s= adpRandSVD(A, tol)
 % s= adpRandSVD(A, tol, P)
@@ -10,16 +11,17 @@ function [Q, B, V] = adpRandSVD(A, tol, p, block_size)
 % [Q, B]= adpRandSVD(A, tol)
 % [Q, B]= adpRandSVD(A, tol, P)
 % [Q, B]= adpRandSVD(A, tol, P, bsize)
-% -tol is the error tolerance for norm(A-U*S*V')/norm(A). We measure the
-%  Frobenius norm. tol is a value less than 1, like 0.1, 0.85.
-% -P is an optional parameter to balance time and accuacy (default value 2).
+% -tol is the error tolerance for norm(A-U*S*V')/norm(A). We measure with
+%  Frobenius norm. tol is a value less than 1, like 0.1, 0.5.
+% -P is an optional parameter to balance time and accuacy (default value 1).
 %  With large P, the accuracy increases with runtime overhead.
 % -bsize is the number of rows/columns processed as a block (default value
 %  20).
-% -Q and B forms a rank-k approximation of A, where Q is an mxk orthonormal 
+% -Q and B form a rank-k approximation of A, where Q is an mxk orthonormal 
 %  matrix, and B is a kxn matrix (A is of mxn).
-%Algorithm: a blocked randomized algorithm randQB_EI for auto-rank problem.
-%  The resulting low-rank approximation guarantees the error is within tol.
+%Algorithm: a blocked randomized algorithm randQB_EI for the fixed-precision 
+%  problem of low-rank matrix approximation.
+%  The result guarantees the relative error of approximation is within tol.
 %  To avoid large computing cost and inaccuracy, it may terminate after a 
 %  maximum rank is attained. 
 %                  --by Wenjian Yu, Nov. 2016
@@ -28,7 +30,7 @@ if nargin<2,
     disp('Less than 2 input arguments');
     return;
 elseif nargin<3
-    p= 2;
+    p= 1;
     block_size=20;
 elseif nargin<4
     block_size=20;
